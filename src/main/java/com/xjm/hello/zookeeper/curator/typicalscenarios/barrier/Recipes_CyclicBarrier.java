3575e98acfa5c25fbcd6b61cf3b7f4b656a34668
Jm <854377742@qq.com>
2020-08-17 16:19:40 +0800

    curator典型场景之分布式 Barrier

---
+++ src/main/java/com/xjm/hello/zookeeper/curator/typicalscenarios/barrier/Recipes_CyclicBarrier.java
@@ -0,0 +1,34 @@
+package com.xjm.hello.zookeeper.curator.typicalscenarios.barrier;
+import java.io.IOException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 使用 CyclicBarrier 模拟一个赛跑比赛
+ */
+public class Recipes_CyclicBarrier {
+
+	public static CyclicBarrier barrier = new CyclicBarrier( 3 );
+	public static void main( String[] args ) throws IOException, InterruptedException {
+		ExecutorService executor = Executors.newFixedThreadPool( 3 );
+		executor.submit( new Thread( new Runner( "1号选手" ) ) );
+		executor.submit( new Thread( new Runner( "2号选手" ) ) );
+		executor.submit( new Thread( new Runner( "3号选手" ) ) );
+		executor.shutdown();
+	}
+}
+class Runner implements Runnable {
+	private String name;
+	public Runner( String name ) {
+		this.name = name;
+	}
+	@Override
+	public void run() {
+		System.out.println( name + " 准备好了." );
+		try {
+			Recipes_CyclicBarrier.barrier.await();
+		} catch ( Exception e ) {}
+		System.out.println( name + " 起跑!" );
+	}
+}
